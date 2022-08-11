9de54392bace4e61c67e5e584529c4b7a4d14c91
LashaPeikrishvili <lasha.peikrishvili17b@geolab.edu.ge>
2022-08-11 04:32:22 +0400

    api is done

--- main.js
+++ main.js
@@ -30,22 +30,90 @@ let sliderDiv = document.querySelector(".rightBottomSliderSection");
 sliderDiv.style.backgroundImage = `url(${images[count]})`
 articles.innerHTML = `${headers[count]}`
 
-next.addEventListener("click", function(){
+next.addEventListener("click", function () {
     console.log(count);
     count++
-   if(count > images.length-1){
-    count = 0
-   }
-   sliderDiv.style.backgroundImage = `url(${images[count]})`
-   articles.innerHTML = `${headers[count]}`
+    if (count > images.length - 1) {
+        count = 0
+    }
+    sliderDiv.style.backgroundImage = `url(${images[count]})`
+    articles.innerHTML = `${headers[count]}`
 })
 
-previus.addEventListener("click", function(){
+previus.addEventListener("click", function () {
     count--
     console.log(count);
-    if(count < 0){
-        count = images.length-1
+    if (count < 0) {
+        count = images.length - 1
     }
-   sliderDiv.style.backgroundImage = `url(${images[count]})`
-   articles.innerHTML = `${headers[count]}`
+    sliderDiv.style.backgroundImage = `url(${images[count]})`
+    articles.innerHTML = `${headers[count]}`
 })
+
+//api
+//let apiBox = document.querySelector(".apiBox")
+const getInfo = async () => {
+    try {
+        const info = await fetch("api.json").then(_ => _.json())
+        console.log(info);
+        let cardBox = document.querySelector(".cardBox")
+        info.map((item) => {
+            console.log(item.txt1);
+            let card = document.createElement("div")
+            cardBox.appendChild(card)
+            let image = document.createElement("img")
+            let p1 = document.createElement("p")
+            let p2 = document.createElement("p")
+            card.append(image, p1, p2)
+            image.setAttribute("src", item.img)
+            image.classList.add("grid-image")
+            p1.innerHTML = item.txt1;
+            p2.innerHTML = item.txt2;
+            p2.style.marginTop = "40px"
+            p1.style.marginTop = "20px"
+
+            // let box = document.createElement(".div")
+            // document.querySelector(".apiBox").appendChild(box)
+            // box.classList.add("box")
+            // box.style.display = "flex"
+            // box.style.justifyContent = 'space-between'
+            // box.style.width = '400px'
+            // box.style.backgroundColor = 'pink'
+            // box.style.margin = '5px'
+            // let text1 = document.createElement("p")
+            // text1.innerText = newInfo.txt1[0]
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+getInfo();
+
+window.addEventListener("scroll",function(){
+    if(document.documentElement.scrollTop>=300){
+        section3.style.marginLeft="5%"
+    }
+})
+// let data = await fetch("./api.json").then((Response)=>Response.json()).then((json)=>{data=json})
+// console.log(data);
+
+// let section3=document.querySelector(".section3")
+
+
+// data.map((item)=>{
+//     let card=document.createElement("div")
+//     section3.appendChild(card)
+    // let image=document.createElement("img")
+    // let p1=document.createElement("p")
+    // let p2=document.createElement("p")
+    // card.append(image,p1,p2)
+    // image.setAttribute("src", item.img)
+    // image.classList.add("grid-image")
+    // p1.innerHTML=item.txt1;
+    // p2.innerHTML=item.txt2;
+    // p2.style.marginTop="40px"
+    // p1.style.marginTop="20px"
+
+// })
